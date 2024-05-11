89d9f0d8fd2a5280c3584348f750bb1c482e9afd
Tim Christy <tim@timchristy.io>
2024-05-11 15:16:59 -0700

    Add size() to static arrays

--- arrays/static_array.py
+++ arrays/static_array.py
@@ -4,3 +4,7 @@ class StaticArr:
         self._placeholder = "-!@!-"
         self._array = [self._placeholder] * self.allocated_size
         self._end_point = 0
+
+    def size(self):
+        """Returns size of the array, i.e. number of elements in the array"""
+        return self._end_point + 1
