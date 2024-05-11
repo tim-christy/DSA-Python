ffcb7b7e6039bc2edb10b72d708808e6e162ec6f
Tim Christy <tim@timchristy.io>
2024-05-11 15:17:21 -0700

    Add append()

--- arrays/static_array.py
+++ arrays/static_array.py
@@ -8,3 +8,13 @@ class StaticArr:
     def size(self):
         """Returns size of the array, i.e. number of elements in the array"""
         return self._end_point + 1
+
+    def append(self, val):
+        """Appends val to the array if space is available. Otherwise errors"""
+        if self._end_point != self.allocation_size - 1:
+            self._array[self._end_point] = val
+            self._end_point += 1
+        else:
+            raise IndexError(
+                "Array is full and cannot be added to. Use insert()"
+            )
